23d7ef453a96057db3df3e433b082df81301cfbc
91953 <91953@DESKTOP-MO588CN>
2023-04-22 17:53:26 +0530

    class creation

---
+++ src/OOPs/Human.java
@@ -0,0 +1,95 @@
+package OOPs;
+
+public class Human {
+
+	//characteristics
+	
+	String name;
+	int age;
+	int height;
+	double weight;
+	String birthplace;
+	String occupation;
+	String country;
+	
+	//Behavior
+	
+	public void run(){}
+	public void speak(){}
+	public void walk(){}
+	public void listen(){}
+	public void watch(){}
+	public void talk(){}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Laptop{
+		
+	//characteristics
+	float screensize;
+	String company;
+	String modelname;
+	int ramsize;
+	int harddisksize;
+	String processor;
+	String os;
+	
+	//Behavior
+	public void operations() {}
+	public void gaming() {}
+	public void filming() {}
+	public void websurfing() {}
+	public void downloading() {}
+	
+}
+
+class SportsMan{
+	
+	//characteristics
+	String name;
+	String gamename;
+	
+	//Behavior
+	public void playing() {}
+	public void running() {}
+	
+	
+}
+
+class TestCase{
+	
+	//characteristics
+	int id;
+	String name;
+	String data;
+	String prerequisite;
+	String steps;
+	String expectedoutput;
+	String acutaloutput;
+	String result;
+	
+	//Behavior
+	public void fucntionalTestcase() {}
+	public void non_fucntionalTestcase() {}
+	public void positiveTestcase() {}
+	public void negativeTestcase() {}
+}
+
+class car{
+	
+	//characteristics
+	String brand;
+	int speed;
+	int mileage;
+	int wheel;
+	
+	//Behavior
+	public void driving() {}
+	public void braking() {}
+	public void reverse() {}
+	public void autopilot() {}
+}
