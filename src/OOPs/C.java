9f7c8f8d227e3f82dfeddbce998da1aa1e2ea7f5
91953 <91953@DESKTOP-MO588CN>
2023-05-05 01:23:56 +0530

    C class

---
+++ src/OOPs/C.java
@@ -0,0 +1,14 @@
+package OOPs;
+
+public class C {
+	
+		int i=10;
+		static int j=20;
+		public void CoreMethod() {
+			System.out.println("C.coremethod");
+		}
+		public static void sharedMethod() {
+			System.out.println("C.SharedMethod");
+		}
+
+}
