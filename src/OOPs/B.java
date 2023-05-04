3e410260bf266c89e03c121f847925e0010d9d4b
91953 <91953@DESKTOP-MO588CN>
2023-05-05 01:23:06 +0530

    Bclass

---
+++ src/OOPs/B.java
@@ -0,0 +1,14 @@
+package OOPs;
+
+public class B {
+	
+	int k = 23;
+	static int m = 33; 
+	public void CoreMethod() {
+		System.out.println("B.coremethod");
+	}
+	public static void sharedMethod() {
+		System.out.println("B.SharedMethod");
+	}
+
+}
