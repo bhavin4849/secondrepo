7f52532770f383b781bd78fb474d8da122f0ff64
91953 <91953@DESKTOP-MO588CN>
2023-05-05 01:42:08 +0530

    Assignment:
    create 2 methods one should be static and one non static method

---
+++ src/OOPs/example.java
@@ -0,0 +1,14 @@
+package OOPs;
+
+public class example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		example e = new example();
+		
+	}
+
+}
+class demo{
+	int i;
+}
