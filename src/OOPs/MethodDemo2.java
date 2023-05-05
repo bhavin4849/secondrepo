60d75e04b4b324d127fe1eeff11defe80ec27a44
91953 <91953@DESKTOP-MO588CN>
2023-05-06 00:38:12 +0530

    create method with int return type
    create 2 methods one should be static and one non static method

---
+++ src/OOPs/MethodDemo2.java
@@ -0,0 +1,19 @@
+package OOPs;
+
+public class MethodDemo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public int Demo1(int i, int j) 
+	{
+		int sum = i+j;
+		return sum;
+	}
+	static int Demo2(int k, int m) 
+	{
+		int mul = k*m;
+		return mul;
+	}
+}
