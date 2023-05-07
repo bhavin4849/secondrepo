bdb33b25294121002f37ac65be305665d1424cf8
91953 <91953@DESKTOP-MO588CN>
2023-05-07 20:34:18 +0530

    Assignment

---
+++ src/OOPs/Method7.java
@@ -0,0 +1,15 @@
+package OOPs;
+
+public class Method7 {
+
+	public static void main(String[] args) {
+		Method7 m =new Method7();
+		System.out.println(m.fun()); 
+
+	}
+	int fun()
+	{
+		return 20;
+	} 
+
+}
