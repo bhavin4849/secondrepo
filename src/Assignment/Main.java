87549b9380fbbfe90b7e1d0f3b3598290ca52fff
91953 <91953@DESKTOP-MO588CN>
2023-05-11 01:57:42 +0530

    assignment

---
+++ src/Assignment/Main.java
@@ -0,0 +1,15 @@
+package Assignment;
+
+public class Main {
+
+	private static int x;
+	public static void main(String[] args) {
+		System.out.println(fun()); 
+
+	}
+
+	static int fun() {
+		
+		return ++x;
+		} 
+}
