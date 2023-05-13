fb3a95c9e703f7adf5a3fc7db6df39b757012cca
91953 <91953@DESKTOP-MO588CN>
2023-05-14 01:59:21 +0530

    Write static method and check visibility with private, default, protected and public  3

---
+++ src/Pack2/AccessModifier9.java
@@ -0,0 +1,11 @@
+package Pack2;
+import Pack1.AccessModifier7;
+public class AccessModifier9 extends AccessModifier7{
+
+	public static void main(String[] args) {
+		AccessModifier7 a7 =new AccessModifier7();
+		a7.method2();
+		a7.method4();
+	}
+
+}
