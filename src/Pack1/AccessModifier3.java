765251e9c2264ae1b014a38644f0152572c3b9de
91953 <91953@DESKTOP-MO588CN>
2023-05-13 18:35:21 +0530

    AccessModifier3

---
+++ src/Pack1/AccessModifier3.java
@@ -0,0 +1,14 @@
+package Pack1;
+
+public class AccessModifier3 {
+
+	public static void main(String[] args) {
+		AccessModifier1 a1 =new AccessModifier1();
+		a1.method1();
+		a1.method2();
+		a1.method3();
+		a1.method4();
+		
+	}
+	
+}
