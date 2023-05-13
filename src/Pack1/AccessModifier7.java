a241760d187b487b7d06132a0d551d8c0e50427b
91953 <91953@DESKTOP-MO588CN>
2023-05-14 01:53:01 +0530

    Write static method and check visibility with private, default, protected and public 1

---
+++ src/Pack1/AccessModifier7.java
@@ -0,0 +1,21 @@
+package Pack1;
+
+public class AccessModifier7 {
+
+	public static void main(String[] args) {
+
+	}
+
+	static void method1() {
+		System.out.println("Default static method");
+	}
+	public static void method2() {
+		System.out.println("public static method");
+	}
+	private static void method3() {
+		System.out.println("private static method");
+	}
+	protected static void method4() {
+		System.out.println("protected static method");
+	}
+}
