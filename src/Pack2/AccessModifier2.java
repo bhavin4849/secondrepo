fe3e8e404479e432e4e93a98722abf051f166026
91953 <91953@DESKTOP-MO588CN>
2023-05-13 18:33:15 +0530

    AcceeModifier2

---
+++ src/Pack2/AccessModifier2.java
@@ -0,0 +1,16 @@
+package Pack2;
+import Pack1.AccessModifier1;
+public class AccessModifier2 extends AccessModifier1{
+
+	public static void main(String[] args) {
+
+		AccessModifier1 ac1 =new AccessModifier1();
+		ac1.method1();
+		ac1.method2();
+		ac1.method3();
+		ac1.method4();
+	
+	}
+	
+
+}
