8860230b8b79f283fb392d4f8f40f59694ca6303
91953 <91953@DESKTOP-MO588CN>
2023-05-14 21:44:39 +0530

    Create classes with all access modifier and check behaviour  1

---
+++ src/Pack1/Class1.java
@@ -0,0 +1,18 @@
+package Pack1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+	
+	}
+}
+ class Class4{
+	
+}
+//We can't create class as a private and protected.
+//protected class Class5{
+	
+//}
+//private class Class6{
+	
+//}
