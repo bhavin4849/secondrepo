b69cdfcf19bf02f01ae2baf52148ce2dc1aa22b3
91953 <91953@DESKTOP-MO588CN>
2023-05-14 21:00:06 +0530

    Write instance variable and check visibility with private, default, protected and public  2

---
+++ src/Pack1/AccessModifier5.java
@@ -0,0 +1,12 @@
+package Pack1;
+public class AccessModifier5{
+	
+	public static void main(String[] args) {
+		AccessModifier4 a4 =new AccessModifier4();
+		System.out.println(a4.i);
+		System.out.println(a4.c);
+		System.out.println(a4.a);
+		a4.method();
+	}
+
+}
