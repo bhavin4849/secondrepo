3a1a1f985e79fa8124efd37ff301e8cfc7eb33a5
91953 <91953@DESKTOP-MO588CN>
2023-05-14 21:47:02 +0530

    Create classes with all access modifier and check behaviour  3

---
+++ src/Pack2/Class3.java
@@ -0,0 +1,11 @@
+package Pack2;
+import Pack1.Class1;
+import Pack1.Class4;
+public class Class3 {
+	public static void main(String[] args) {
+		Class1 c1 =new  Class1(); 
+		Class4 c4 = new Class4(); 
+
+	}
+
+}
