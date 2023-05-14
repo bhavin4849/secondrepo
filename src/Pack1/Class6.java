146c139413d8834b02ac0a4b6edf82f43c31c45b
91953 <91953@DESKTOP-MO588CN>
2023-05-14 22:02:06 +0530

    Write static variable and check visibility with private, default, protected and public  2

---
+++ src/Pack1/Class6.java
@@ -0,0 +1,12 @@
+package Pack1;
+
+public class Class6 {
+
+	public static void main(String[] args) {
+		System.out.println(Class5.i);
+		System.out.println(Class5.j);
+		System.out.println(Class5.l);
+
+	}
+
+}
